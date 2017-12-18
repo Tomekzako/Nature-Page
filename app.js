c2517fddabc3be0a3732027fa9c748eeae90aa2e
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-12-18 15:28:56 +0100

    Finish navigation icon and section

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ $(function () {
 
     const $navBtn = $('.navigation__button');
 
-    $navBtn.on('click', () => {
+    $navBtn.on('click', function() {
         $(".navigation__background").toggleClass("navigation__background--scale");
+        $(this).toggleClass("navigation__button--checked");
         $('.navigation__nav').fadeToggle("slow","linear");
+//        if($(".navigation__background").hasClass("navigation__background--scale")) {
+//            
+//           }
         
     });
     
